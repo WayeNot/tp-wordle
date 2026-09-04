3468421b3ecb80cab3079e5811a28f8634268eb1
baymeric <aymericbeaune917@gmail.com>
2026-09-04 15:45:28 +0200

    [ :rocket: ] - Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,41 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+"use client";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react';
+import { BiSolidArchiveOut, BiStats } from 'react-icons/bi';
+import { FaQuestion } from 'react-icons/fa';
+import { IoSettingsSharp } from 'react-icons/io5';
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+import HowToPlay from '../src/components/HowToPlay';
+
+export default function App() {
+    const [howToPlay, setHowToPlay] = useState(false);
+
+    return (
+        <div className="App">
+            <div className="w-screen bg-gray-800 p-5">
+                <nav className="flex items-center justify-between w-1/3 m-auto">
+                    <div className="flex items-center gap-3 text-white text-[35px]">
+                        <BiSolidArchiveOut className="bg-gray-700/60 p-2 rounded-[5px] cursor-pointer hover:bg-gray-700 transition duration-500" />
+                        <BiStats className="bg-gray-700/60 p-2 rounded-[5px] cursor-pointer hover:bg-gray-700 transition duration-500" />
+                    </div>
+                    <div className="flex items-center gap-6 uppercase text-white font-bold text-[18px] ">
+                        <div className="flex items-center gap-2">
+                            <div className="w-[30px] h-[30px] bg-green-600 rounded-[7px] flex items-center justify-center">L</div>
+                            <p>e</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <div className="w-[30px] h-[30px] bg-orange-400 rounded-[7px] flex items-center justify-center">M</div>
+                            <p>o</p>
+                            <p>t</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-3 text-white text-[35px]">
+                        <FaQuestion onClick={() => setHowToPlay(true)} className="bg-gray-700/60 p-2 rounded-[5px] cursor-pointer hover:bg-gray-700 transition duration-500" />
+                        <IoSettingsSharp className="bg-gray-700/60 p-2 rounded-[5px] cursor-pointer hover:bg-gray-700 transition duration-500" />
+                    </div>
+                </nav>
+            </div>
+            {howToPlay && <HowToPlay/>}
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    );
 }
-
-export default App
---
+++ src/components/HowToPlay.tsx
@@ -0,0 +1,14 @@
+import { FaWindowClose } from "react-icons/fa";
+
+export default function HowToPlay() {
+    return (
+        <div className="bg-black/60">
+            <div>
+                <div>
+                    <h2>Comment jouer ?</h2>
+                    <FaWindowClose className="text-white"/>
+                </div>
+            </div>
+        </div>
+    )
+}
